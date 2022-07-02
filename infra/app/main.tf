725e9dddd97ac781cc6f551b9dd0002599b86f6e
Jesse Hills <contact@jessehills.io>
2022-07-03 00:46:18 +0800

    added autoscaling for app

--- infra/app/main.tf
+++ infra/app/main.tf
@@ -116,7 +116,7 @@ resource "aws_ecs_service" "app" {
   name            = "servian-tech-app"
   cluster         = aws_ecs_cluster.app.id
   task_definition = aws_ecs_task_definition.app.arn
-  desired_count   = 2
+  desired_count   = 1
   launch_type     = "FARGATE"
 
   load_balancer {
@@ -132,6 +132,32 @@ resource "aws_ecs_service" "app" {
 
   lifecycle {
     ignore_changes = [desired_count]
+  }
+}
+
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 4
+  min_capacity       = 1
+  resource_id        = "service/${aws_ecs_cluster.app.name}/${aws_ecs_service.app.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ecs_policy" {
+  name               = "AutoScaleApp"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value       = 80
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 300
   }
 }
 
